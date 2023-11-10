0ae818c9e7b4279e95938d461d4fca5fbc318e60
Xavier Francisco <xavierfrancisco353@gmail.com>
2023-11-10 22:20:42 +0200

    Added models

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core import models
+from users.models import User
 
-# Register your models here.
+
+admin.site.register(models.Inquerito)
+admin.site.register(models.IdentificacaoAggFamiliar)
+admin.site.register(models.CaracteristicasAggFamiliar)
+admin.site.register(User)
